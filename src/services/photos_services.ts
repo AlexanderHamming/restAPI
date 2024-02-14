204c78db881f0ad96f4c98979cc139e90f87b6f0
Alexander Hamming <alexanderhamming17@gmail.com>
2024-02-14 15:33:43 +0100

    error because changed name

--- src/services/photos_services.ts
+++ src/services/photos_services.ts
@@ -2,31 +2,40 @@ import prisma from "../prisma";
 import { createPhoto, updatePhoto, photoId } from "../types/photo_types";
 
 
-export const CreatePhoto = async (data: createPhoto) => {
+export const CreatePhoto = async (userId: number, data: createPhoto) => {
 	return await prisma.photo.create({
-		data,
-	})
+	data: {
+		...data,
+		userId,
+	},
+	});
 }
 
-export const GetPhotos = async () => {
-return await prisma.photo.findMany();
+export const GetPhotos = async (userId: number) => {
+return await prisma.photo.findMany({
+	where: {
+		userId,
+	},
+});
 }
 
-export const GetPhoto = async (photoId: number) => {
+export const GetPhoto = async (userId: number, photoId: number) => {
 return await prisma.photo.findUniqueOrThrow({
 	where: {
 		id: photoId,
-	}
-})
+		userId,
+	},
+});
 }
 
-export const patchPhoto = async (photoId: number, data: updatePhoto ) => {
+export const patchPhoto = async (userId: number, photoId: number, data: updatePhoto ) => {
 	return prisma.photo.update({
 		where: {
 			id: photoId,
+			userId,
 		},
 		data,
-	})
+	});
 }
 
 
--- src/services/user_services.ts
+++ src/services/user_services.ts
@@ -8,3 +8,16 @@ export const CreateUser = async (data: createUser) => {
 }
 
 
+export const GetUserInfo = async (userId: number) => {
+	return prisma.user.findUnique({
+		where: { id: userId}
+	})
+}
+
+
+export const patchUser = async (userId: number, data: updateUser) => {
+	return await prisma.user.update({
+		where: { id: userId },
+		data,
+	});
+}
